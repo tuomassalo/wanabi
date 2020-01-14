5902e4d357a0a3f7f4dae8aa3c19ffad857c9d1b
Tuomas Salo <tuomas.salo@iki.fi>
2020-01-14 17:18:28 +0200

    lock react client package versions

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import logo from './logo.svg';
+import './App.css'
+// import {Game} from './engine-src/game'
 
 const App: React.FC = () => {
+  // const g = new Game({playerNames: ['foo', 'bar']})
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
