4beb0af32d389aeff33f17bc6452abe3cebc0fa7
Tuomas Salo <tuomas.salo@iki.fi>
2020-02-12 23:44:16 +0200

    show actionability

--- client/src/WMysteryCard.tsx
+++ client/src/WMysteryCard.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-import {MaskedCard, Card} from 'wanabi-engine/dist/card'
+import {MaskedCard, Card, TActionability} from 'wanabi-engine/dist/card'
 
 export default class WMysteryCard extends React.Component<{card: MaskedCard}> {
   render() {
     const {color, num, possibleCards} = this.props.card
-    const allPossibleCards: {card: Card; prob: number}[] = (possibleCards || []).flatMap(pc =>
-      Array(pc.count).fill({card: Card.fromValueString(pc.value), prob: pc.prob / pc.count}),
+    const allPossibleCards: {card: Card; prob: number; actionability?: TActionability}[] = (
+      possibleCards || []
+    ).flatMap(pc =>
+      Array(pc.count).fill({
+        card: Card.fromValueString(pc.value),
+        prob: pc.prob / pc.count,
+        actionability: pc.actionability,
+      }),
     )
     if (allPossibleCards.length > 1 && allPossibleCards.length < 10) {
       // if (num) {
@@ -21,10 +27,18 @@ export default class WMysteryCard extends React.Component<{card: MaskedCard}> {
       // } else {
       // show a collection of miniature cards
       return (
-        <div className={allPossibleCards.length > 4 ? 'WUpToNinePossibleCards' : 'WUpToFourPossibleCards'}>
+        <div
+          className={
+            // `WCard-${this.props.card.actionability} ` +
+            allPossibleCards.length > 4 ? 'WUpToNinePossibleCards' : 'WUpToFourPossibleCards'
+          }
+        >
           {/* NB: bogus index, might break animations */}
           {allPossibleCards.map((pc, idx) => (
-            <div key={idx} className={`WCard WColor-${pc.card.color}`}>
+            <div
+              key={idx}
+              className={`WCard WColor-${pc.card.color} WCard-${pc.actionability || this.props.card.actionability}`}
+            >
               {pc.card.num}
             </div>
           ))}
@@ -34,7 +48,11 @@ export default class WMysteryCard extends React.Component<{card: MaskedCard}> {
     } else {
       // too many possible cards - we only know color or num or neither
 
-      return <div className={`WCard WMysteryCard WColor-${color}`}>{num || '\xa0'}</div>
+      return (
+        <div className={`WCard WCard-${this.props.card.actionability} WMysteryCard WColor-${color}`}>
+          {num || '\xa0'}
+        </div>
+      )
     }
   }
 }
