078bd0571b71802c2c1888893dc4ac20afa8fd8b
Tuomas Salo <tuomas.salo@iki.fi>
2020-07-16 00:27:50 +0300

    difficulty params test

---
+++ engine/test/difficulty-params.spec.ts
@@ -0,0 +1,22 @@
+import {createDeck} from './helpers'
+import {Game} from '../src/game'
+
+function createTestGame() {
+  const g = new Game({
+    from: 'NEW_TEST_GAME',
+    playerNames: ['Alpha', 'Omega'],
+    deck: createDeck(
+      // p0 p1 p0 p1 p0 p1 (p0 plays and p1 discards)
+      `  A1 B1 A1 B1 A2 C1
+         A3 A4 A4 B5 D1 X1`,
+    ),
+  })
+  return g
+}
+
+describe('Difficulty params', () => {
+  it('are returned', () => {
+    const g = createTestGame()
+    expect(g.getState(g.players[0].id).difficultyParams).toEqual({maxHintCount: 8, maxWoundCount: 3})
+  })
+})
